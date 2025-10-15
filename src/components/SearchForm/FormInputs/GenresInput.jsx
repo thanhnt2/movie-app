6e387700dd79057b9c80a4394fbfdda2de9b09c4
Upin <otlthanh@gmail.com>
2025-10-15 09:32:08 +0700

    feat: add genres input

---
+++ src/components/SearchForm/FormInputs/GenresInput.jsx
@@ -0,0 +1,34 @@
+import { useWatch } from "react-hook-form";
+import useFetch from "../../../hooks/useFetch";
+
+const GenresInput = ({ control, onChange, value = [] }) => {
+  const mediaType = useWatch({ name: "mediaType", control });
+  const { data } = useFetch(
+    { url: `/genre/${mediaType}/list` },
+    { enabled: mediaType },
+  );
+  console.log({ data, mediaType });
+
+  return (
+    <div className="flex flex-wrap gap-1">
+      {(data.genres || []).map((genre) => (
+        <p
+          key={genre.id}
+          className={`cursor-pointer rounded-lg border px-2 py-1 ${value.includes(genre.id) ? "bg-black text-white" : ""}`}
+          onClick={() => {
+            let newValue = [...value];
+            if (value.includes(genre.id)) {
+              newValue = newValue.filter((id) => id !== genre.id);
+            } else {
+              newValue = [...newValue, genre.id];
+            }
+            onChange(newValue);
+          }}
+        >
+          {genre.name}
+        </p>
+      ))}
+    </div>
+  );
+};
+export default GenresInput;
---
+++ src/components/SearchForm/index.jsx
@@ -0,0 +1,33 @@
+import { useForm } from "react-hook-form";
+import FormField from "./FormField";
+import MediaTypeInput from "./FormInputs/MediaTypeInput";
+import GenresInput from "./FormInputs/GenresInput";
+
+const SearchForm = () => {
+  const { handleSubmit, control, register } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("dataform" + JSON.stringify(data));
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormField
+          name="mediaType"
+          label="Media Type"
+          control={control}
+          Component={MediaTypeInput}
+        />
+        <FormField
+          name="genres"
+          label="Genres"
+          control={control}
+          Component={GenresInput}
+        />
+        <br />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
+export default SearchForm;
